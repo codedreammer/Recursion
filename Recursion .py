a011ef2ed8aa27938d7bf47114cd0e9c579b99ee
codedreammer <nkanal38@gmail.com>
2024-07-18 21:22:51 +0530

    add the file

--- Recursion .py
+++ Recursion .py
@@ -1,7 +1,19 @@
-def factorial(n):
-    if(n==0 or n==1):
-        return 1
-    else:
-        return n * factorial(n-1)
+#def factorial(n):
+    #if(n==0 or n==1):
+   #     return 1
+  #  else:
+ #       return n * factorial(n-1)
 
-print(factorial(5))
+#print(factorial(5))
+
+def fibbonacci(f):
+    if(f<=1):
+        return f
+    else:
+          return (fibbonacci(f-1) + fibbonacci(f-2))
+f = int(input("enter the value of f\n"))
+for i in range(f):
+    print(fibbonacci(i))
+
+
+
